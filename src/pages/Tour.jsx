f77e356680b51801334c8dff743e746eed1341bb
Aristide <idealy371@gmail.com>
2022-03-03 13:27:15 +0300

    fix browser routes

--- src/pages/Tour.jsx
+++ src/pages/Tour.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
-import { BrowserRouter as   Outlet } from "react-router-dom"
+import {  Outlet } from "react-router-dom"
 
 function Tour() {
     return  <Outlet />
     
 }
-
 
 export default Tour
