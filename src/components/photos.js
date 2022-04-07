b3995c6bee64cb85592868a693abfd79f543e735
Aristide <idealy371@gmail.com>
2022-04-07 11:02:42 +0300

    ajustement taille images

--- src/components/photos.js
+++ src/components/photos.js
@@ -4,87 +4,104 @@ export const photos = [
     {
       src: "https://files.fm/thumb_show.php?i=aqe8ctrsc",
       width: 5,
-      height: 7
+      height: 5,
+      title: "Bemaraha Tsingy Gate "
     },
     {
       src: "https://files.fm/thumb_show.php?i=c43an2ewv",
-      width: 1,
-      height: 1
+      width: 4,
+      height: 3,
+      title: "Sunset in Itampolo beach"
     },
     {
       src: "https://files.fm/thumb_show.php?i=sd6t653yn",
-      width: 3,
-      height: 4
+      width: 4,
+      height: 3,
+      title: "Lemur Catta at Kirindy parc"
     },
     {
       src: "https://files.fm/thumb_show.php?i=rkgqr6r98",
-      width: 3,
-      height: 4
+      width: 4,
+      height: 3,
+      title: "Andringitra Hill"
     },
     {
       src: "https://files.fm/thumb_show.php?i=pa4uqgnt7",
-      width: 3,
-      height: 4
+      width: 5,
+      height: 5,
+      title: "Cryptaferox or Fossa"
     },
     {
       src: "https://files.fm/thumb_show.php?i=zrd93b25n",
-      width: 4,
-      height: 3
+      width: 2,
+      height: 3,
+      title: "Baobab of Morondava"
     },
     {
       src: "https://files.fm/thumb_show.php?i=u9xbemwh5",
-      width: 3,
-      height: 4
+      width: 4,
+      height: 3,
+      title: "Isalo montain"
     },
     {
       src: "https://files.fm/thumb_show.php?i=w9p2tcn3y",
       width: 4,
-      height: 3
+      height: 3,
+      title: "Isalo landscape"
     },
     {
       src: "https://files.fm/thumb_show.php?i=qy93tnp8p",
       width: 4,
-      height: 3
+      height: 2,
+      title: "Zebu market of Ambalavao"
     },
     {
       src: "https://files.fm/thumb_show.php?i=4qyu2b33b",
-      width: 4,
-      height: 3
+      width: 2,
+      height: 3,
+      title: "Ranomafana's waterfall"
     },
     {
       src: "https://files.fm/thumb_show.php?i=89eq4ew73",
-      width: 4,
-      height: 3
+      width: 2,
+      height: 3,
+      title: "Comet butterfly"
     },
     {
       src: "https://files.fm/thumb_show.php?i=fywmb9m5n",
-      width: 4,
-      height: 3
+      width: 2,
+      height: 3,
+      title: "Endemic chameleon of Madagascar"
     },
     {
       src: "https://files.fm/thumb_show.php?i=6g64ww5ha",
       width: 4,
-      height: 3
+      height: 3,
+      title: "Ifaty beach sunset"
     },
     {
       src: "https://files.fm/thumb_show.php?i=qsmwyug46",
       width: 4,
-      height: 3
+      height: 2,
+      title: "Lemur"
     },
     {
       src: "https://files.fm/thumb_show.php?i=jec4zpt85",
-      width: 4,
-      height: 3
+      width: 2,
+      height: 3,
+      title: "Baobab avenue"
     },
     {
       src: "https://files.fm/thumb_show.php?i=7mgchtvz4",
       width: 4,
-      height: 3
+      height: 3,
+      title: "Andringitra hill"
     },
     {
       src: "https://files.fm/thumb_show.php?i=7ze6pvqr9",
       width: 4,
-      height: 3
+      height: 3,
+      title: "Nosy komba"
     },
   ];
   
