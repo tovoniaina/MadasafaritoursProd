06ed09cd6cebbd6610720fd98b74968a8da78fd8
Aristide <idealy371@gmail.com>
2022-06-10 09:52:16 +0300

    implementation mixpanel

--- src/index.js
+++ src/index.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 import './css/App.css';
 import App from './App';
 import 'react-toastify/dist/ReactToastify.min.css';
-
+import mixpanel from 'mixpanel-browser';
 import reportWebVitals from './reportWebVitals';
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+mixpanel.init('5736b4bf2cb97d5fde82f47f4ab2acab', {debug: true}); 
+mixpanel.track('Visit Madasafaritours landing page', {
+  'source': "Pat's affiliate site",
+  'Opted out of email': true,
+});
 
 
 
