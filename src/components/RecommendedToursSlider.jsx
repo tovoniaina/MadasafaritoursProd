45a0485e6660a0e2d753b3b9ddebe9f45ca33145
Aristide <idealy371@gmail.com>
2022-07-05 10:42:55 +0300

    adding reviews platform

--- src/components/RecommendedToursSlider.jsx
+++ src/components/RecommendedToursSlider.jsx
@@ -9,13 +9,9 @@ function RecommendedToursSlider() {
       <Helmet>
       <script src="https://apps.elfsight.com/p/platform.js" defer></script>
       </Helmet>
-      <div class="elfsight-app-3ea49f29-5c72-41c1-a445-a8d10b2c9532"></div>
+      <div class="elfsight-app-f2b71d9c-537e-4661-b9d3-eaffb030b3f3"></div>
     </div>
   )
 }
 
 export default RecommendedToursSlider
-
-
-
-
