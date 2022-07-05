f8b3c6e497a7c124c37bde7e7fec7e1cd81ca357
Aristide <idealy371@gmail.com>
2022-07-05 10:21:40 +0300

    fix mixpanel

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
-mixpanel.init('5736b4bf2cb97d5fde82f47f4ab2acab', {debug: true}); 
+mixpanel.init('700c6b5fc208d4aae168933b59958a6c', {debug: true}); 
 mixpanel.track('Visit Travelia Safari landing page', {
   'source': "Travelia safari source code",
   'Opted out of email': true,
